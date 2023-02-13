ba9a5518bb4ca43fa6ee781e336b7f67c96607ec
Hadeel Ammari <80487488+hadeelammari@users.noreply.github.com>
2023-02-13 15:16:46 -0800

    Update admin.js
    Hi Syrinthia
    Please take a look at the code changes I have made. There were many syntax errors and your fetch command was not formed correctly.
    Thank you

--- admin.js
+++ admin.js
@@ -8,39 +8,34 @@ async function main() {
 
 function renderBook(book) {
     //select the root
-let root = document.getElementById("root")
-let listItem = document.createElement("li")
-let qtyInput = document.createElement("input")
-let saveBtn = document.createElement("button")
+    let root = document.getElementById("root")
+    let listItem = document.createElement("li")
+    let qtyInput = document.createElement("input")
+    let saveBtn = document.createElement("button")
 
-listItem.innHTML = '$(book.title)'
-qtyInput.setAttribute('value', '${book.quantity}')
-saveBtn.textContent = "Save"
+    listItem.innerHTML = book.title
+    qtyInput.setAttribute('value', `${book.quantity}`)
+    saveBtn.textContent = "Save"
 
-saveBtn.addEventListener('click', () => {
-     //Add code here, when the button gets clicked
-    // We need to capture the quatity
-    // We need to call an API endpoint to update the book quantity
-    fetch('http://localhost:3001/listBooks',{
-        method: "PATCH",
-        headers: {
-            'Content-Type' : 'application/json'
-        },
+    saveBtn.addEventListener('click', () => {
+        //Add code here, when the button gets clicked
+        // We need to capture the quatity
+        // We need to call an API endpoint to update the book quantity
+        fetch('http://localhost:3001/updateBook', {
+            method: "PATCH",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                'id': book.id,
+                'quantity': qtyInput.value
+            })
         })
-    
-        body: JSON.stringify({
-            id: book.id,
-            quantity: qtyInput.value
-})
     })
 
-   
-   
-   
 
-
-listItem.append(qtyInput, saveBtn)
-root.append(listItem)
+    listItem.append(qtyInput, saveBtn)
+    root.append(listItem)
 }
 
 main()
